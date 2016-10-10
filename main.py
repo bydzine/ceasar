f21eb4ddf7166654a28b14f984c77ae2fb95a8d0
Laura Hizer <lhizer@gmail.com>
2016-10-10 01:05:16 -0500

    Ceasar part 1 and part 2
    redid ceasar part 1 to add more info now that request handler is better understood
    Adding Sign up which is part 2 of Caesar

--- main.py
+++ main.py
@@ -1,25 +1,129 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import re
+			
 
+# html boilerplate header and footer
+header = """ 
+<!DOCTYPE html>
+<html>
+<head>
+		<style type="text/css">
+			.error{
+				color:red;
+			}
+		</style>
+</head> 
+"""
+form = """
+	<body>
+	<h1>Sign Up</h1>
+		<form method="post">
+			<table>
+				<tr>
+					<td class="label">Username</td>
+					<td>
+						<input name="username" type="text" value="">
+						<span class="error">{erroru}</span>
+					</td>
+				</tr>
+				<tr>
+					<td class="label">Password</td>
+					<td>
+						<input name="password" type="password" required/>
+						<span class="error">{errorp}</span>
+					</td>
+				</tr>
+				<tr>
+					<td class="label">Verify Password</td>
+					<td>
+						<input name="verify" type="password" required/>
+						<span class="error">{errorv}</span>
+					</td>
+					</tr>
+				 <tr>
+          			<td class="label">Email</td>
+					<td>
+						<input name="email" type="email">
+						<span class="error">{errore}</span>
+					</td>
+				</tr>
+			</table>
+			<input type= "submit">
+		</form>
+</body>
+</html>"""
+				
+						
+								
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
 
+	def validate_username(self, username):
+		USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+		if USER_RE.match(username):
+			return username
+		else:
+			return ""
+		
+	def validate_password(self, password):
+		PWD_RE = re.compile(r"^.{3,20}$")
+		if PWD_RE.match(password):
+			return password
+		else:
+			return ""
+
+	def validate_verify(self, password, verify):
+		# will not continue if password is not verified
+		if password == verify:
+			return verify
+
+	def validate_email(self, email):
+		# allow blank email field
+		if not email:
+			return ""
+		EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+		if EMAIL_RE.match(email):
+			return ""
+
+	def get(self):
+		# create dictionary from errors
+		errors = {'erroru':'', 'errorp':'', 'errorv':'', 'errore':''}
+		self.response.write(header + form.format(**errors))
+
+	def post(self):
+		submitted_username = self.request.get("username")
+		submitted_password = self.request.get("password")
+		submitted_verify = self.request.get("verify")
+		submitted_email = self.request.get("email")
+
+
+		username = self.validate_username(submitted_username)
+		password = self.validate_password(submitted_password)
+		verify = self.validate_verify(submitted_password, submitted_verify)
+		email = self.validate_email(submitted_email)
+
+		if (username and password and verify and (email is not None) ):
+			self.redirect('/welcome?username=%s' % username)
+		
+
+		errors = {'erroru':'', 'errorp':'', 'errorv':'', 'errore':''}
+		if not username:
+			errors['erroru'] = "Not a valid username"
+			erroru = True
+		if not password:
+			errors['errorp'] = "Not a valid password"
+			errorp = True
+		if not verify:
+			errors['errorv'] = "Passwords do not match"
+			errorv = True
+	
+		
+		self.response.write(header + form.format(**errors))
+		#self.render('signup.html', username=username, email=email, errors=errors)
+
+#class Welcome(Handler):
+	#def get(self):
+		#username = self.request.get("username")
+		#self.render('welcome.html', username=username)
+		
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+	('/', MainHandler)], debug = True)
